cd62b52cc311f5854a6cd7787cf82ff457424c96
Guillaume Passebon <guillaume.passebon@gmail.com>
2014-07-31 16:43:13 +0200

    VirtualItem

---
+++ wp-store/wp-store/domain/VirtualItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoomlaWpCore.data;
+using SoomlaWpCore;
+using Newtonsoft.Json.Linq;
+namespace SoomlaWpStore.domain
+{
+    public abstract class VirtualItem : SoomlaEntity
+    {
+        public VirtualItem(String Name, String Description, String ItemId) : base(Name, Description, ItemId)
+        {
+            
+            
+        }
+
+        public VirtualItem(JObject jsonObject) : base(jsonObject)
+        {
+        }
+
+        public new JObject toJSONObject()
+        {
+            return base.toJSONObject();
+        }
+
+        public int give(int amount)
+        {
+            return give(amount, true);
+        }
+
+        public abstract int give(int amount, bool notify);
+
+        public int take(int amount)
+        {
+            return take(amount, true);
+        }
+
+        public abstract int take(int amount, bool notify);
+
+        public int resetBalance(int balance)
+        {
+            return resetBalance(balance, true);
+        }
+
+        public abstract int resetBalance(int balance, bool notify);
+
+        public String getItemId()
+        {
+            return mID;
+        }
+
+        private const String TAG = "SOOMLA VirtualItem"; //used for Log messages
+    }
+}
